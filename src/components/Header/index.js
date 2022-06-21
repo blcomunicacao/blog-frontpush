4a705d894a9400a49fa89202efd46ab01335b421
Bruno Lopes <brunolopes@iMac-de-bruno.local>
2022-06-21 11:05:41 -0300

    Styling the Header

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,6 +1,25 @@
+import logo from '../../svg/logo.svg';
+
 const Header = () => {
+
+  const altLogo = "Blog frontpush";
+
   return(
-    <h1>Header.js</h1>
+    <header className="flex-space-between">
+      <div className="logo">
+        <img src={logo} alt={altLogo} />
+      </div>
+
+      <div className="search">
+        <input type="text" name="search" className="input-search" />
+      </div>
+     
+      <ul className="menu">
+        <li><a href="#" className="nav-link">Categories</a></li>
+        <li><a href="#" className="nav-link">About</a></li>
+        <li><a href="#" className="nav-link">Contact</a></li>
+      </ul>
+    </header>
   );
 }
 
